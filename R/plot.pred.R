3ebbe2f92fbab8494fb2468e258d1711ab3133e4
hanneoberman <h.i.oberman@uu.nl>
2022-02-16 18:03:01 +0100

    Remove quickpred plotting function because plot_pred() can do the trick

--- R/plot.pred.R
+++ R/plot.pred.R
@@ -18,7 +18,7 @@ plot_pred <- function(pred, label = FALSE){
     vrb = vrbs,
     ind = matrix(pred, nrow = p*p, byrow = TRUE))
   gg <- ggplot2::ggplot(long, ggplot2::aes(x = prd, y = vrb, label = ind, color = ifelse(ind == 0, "no", "yes"))) +
-    ggplot2::geom_tile(fill = "white", size = 3, width = 0.9, height = 0.9) +
+    ggplot2::geom_tile(fill = "white", size = 1.5*p, width = 0.8, height = 0.8) +
     ggplot2::scale_x_discrete(limits = vrbs, position = "top") +
     ggplot2::scale_y_discrete(limits = rev(vrbs)) +
     ggplot2::scale_color_manual(values = c("yes" = "#006CC2B3", "no" = "#B61A51B3")) +
@@ -31,28 +31,28 @@ plot_pred <- function(pred, label = FALSE){
   return(gg)
 }
 
-# plot quickpred
-#' Title
-#'
-#' @param dat An incomplete data.frame
-#' @param ... Optional additional arguments to `mice::quickpred`
-#'
-#' @return An object of class `ggplot`
-#'
-#' @examples
-#' plot_quickpred(mice::nhanes)
-#' @export
-plot_quickpred <- function(dat, ...){
-  #TODO: make this work with other pred matrices
-  vrbs <- names(dat)
-  p <- length(vrbs)
-  corrs <- data.frame(
-    vrb = rep(vrbs, each = p),
-    prd = vrbs,
-    corr = matrix(round(stats::cov2cor(stats::cov(dat, use =  "pairwise.complete.obs")), 3), nrow = p*p, byrow = TRUE))
-  plot_pred(mice::quickpred(dat, ...)) +
-    ggplot2::geom_text(ggplot2::aes(x = vrb, y = prd, label = paste("r =", corr)), color = "black", data = corrs)
-}
+# # plot quickpred
+# #' Title
+# #'
+# #' @param dat An incomplete data.frame
+# #' @param ... Optional additional arguments to `mice::quickpred`
+# #'
+# #' @return An object of class `ggplot`
+# #'
+# #' @examples
+# #' plot_quickpred(mice::nhanes)
+# #' @export
+# plot_quickpred <- function(dat, ...){
+#   #TODO: make this work with other pred matrices
+#   vrbs <- names(dat)
+#   p <- length(vrbs)
+#   corrs <- data.frame(
+#     vrb = rep(vrbs, each = p),
+#     prd = vrbs,
+#     corr = matrix(round(stats::cov2cor(stats::cov(dat, use =  "pairwise.complete.obs")), 3), nrow = p*p, byrow = TRUE))
+#   plot_pred(mice::quickpred(dat, ...)) +
+#     ggplot2::geom_text(ggplot2::aes(x = vrb, y = prd, label = paste("r =", corr)), color = "black", data = corrs)
+# }
 
 #' Title
 #'
