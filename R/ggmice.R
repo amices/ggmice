1752bc468688a33ca08ef35e68c52bf6b370feb1
hanneoberman <h.i.oberman@uu.nl>
2022-02-23 22:48:06 +0100

    document ggmice function, but failing tests

--- R/ggmice.R
+++ R/ggmice.R
@@ -1,20 +1,59 @@
-ggmice <- function(data = NULL, mapping = aes()) {
+#' Title
+#'
+#' @param data An incomplete dataset or an object of class `mids`
+#' @param mapping A list of aesthetic mappings created with `ggplot2::aes()`
+#'
+#' @return A ggplot object of class `gg`
+#'
+#' @examples
+#' imp <- mice::mice(mice::nhanes, print = FALSE)
+#' ggmice(imp, ggplot2::aes(x = age, y = bmi))
+#' @export
+ggmice <- function(data = NULL, mapping = ggplot2::aes()) {
+  # process inputs
   mapping_args <- names(mapping)
   try(if(!any(c("x", "y") %in% mapping_args)){
-    stop("Provide at least one of the mapping arguments 'x' or 'y'.")
+    stop("At least one of the aes() arguments 'x' or 'y' is required. Cannot create ggmice object without mapping.")
   })
   if(any(c("colour", "fill", "group") %in% mapping_args)){
-    warning("The mapping arguments 'colour', 'fill' and 'group' have a special use in ggmmice() and will be overwritten, maybe use 'shape' or 'linetype' instead?")
+    warning("The aes() arguments 'colour', 'fill' and 'group' have a special use in ggmmice() and will be overwritten. Try using 'shape' or 'linetype' for additional mapping.")
   }
-  # x_mis <- if("x" %in% mapping_args){data$where[, rlang::as_name(mapping$x)]} else FALSE
-  # y_mis <- if("y" %in% mapping_args){data$where[, rlang::as_name(mapping$y)]} else FALSE
-  # xy_mis <- x_mis | y_mis
-  xy_vrb <- c(if("y" %in% mapping_args){rlang::as_name(mapping$y)}, if("y" %in% mapping_args){rlang::as_name(mapping$y)})
-  xy_mis <- rowSums(data$where[, xy_vrb] > 0)
+  # extract variable names from mapping object
+  if(mice::is.mids(data)){
+    vrbs <- names(data$data)
+  } else {
+    vrbs <- names(data)
+  }
+  vrbs_xy <- vrbs[stringr::str_detect(ggplot2::as_label(mapping$x), vrbs) | stringr::str_detect(ggplot2::as_label(mapping$y), vrbs)]
+  # edit data and mapping objects
+  if(mice::is.mids(data)){
+    mice_data <- dplyr::mutate(mice::complete(data, action = "long", include = TRUE),
+                        .imp = factor(.imp, levels = 0:data$m, ordered = TRUE),
+                        .mis = rep(rowSums(as.matrix(data$where[, vrbs_xy])) > 0L, data$m + 1L),
+                        .mis = factor(.mis, levels = c(FALSE, TRUE), labels = c("observed", "imputed"), ordered = TRUE))
+    mice_mapping <- utils::modifyList(mapping, ggplot2::aes(colour = .mis, fill = .mis))
+  } else {
+    mice_data <- dplyr::mutate(data,
+                       .mis = rowSums(is.na(as.matrix(data[, vrbs_xy]))) > 0L,
+                       .mis = factor(.mis, levels = c(FALSE, TRUE), labels = c("observed", "missing"), ordered = TRUE))
+    mice_mapping <- utils::modifyList(mapping, ggplot2::aes(colour = .mis, fill = .mis))
+  }
+  # create plot
+  mice_colors <- c("observed" = "#006CC2B3", "missing" = "#B61A51B3", "imputed" = "#B61A51B3")
+  gg <- ggplot2::ggplot(data = mice_data, mapping = mice_mapping) +
+    ggplot2::scale_color_manual(values = mice_colors, drop = TRUE, name = "") +
+    ggplot2::scale_fill_manual(values = mice_colors, drop = TRUE, name = "") +
+    theme_mice()
 
-  mice_data <- cbind(mice::complete(data, action = "long", include = TRUE),
-                     .mis = factor(xy_mis, levels = c(0,1), labels = c("observed", "imputed"), ordered = TRUE))
-  mice_mapping <- modifyList(mapping, aes(colour = .mis, fill = .mis, group = factor(.imp, ordered = TRUE)))
-  gg <- ggplot2::ggplot(data = mice_data, mapping = mice_mapping)
+  # output
   return(gg)
 }
+
+
+# x_mis <- if("x" %in% mapping_args){data$where[, rlang::as_name(mapping$x)]} else FALSE
+# y_mis <- if("y" %in% mapping_args){data$where[, rlang::as_name(mapping$y)]} else FALSE
+# xy_mis <- x_mis | y_mis
+# mapping_vrb <- c(if("x" %in% mapping_args){rlang::as_name(mapping$x)}, if("y" %in% mapping_args){rlang::as_name(mapping$y)})
+# mice_data <- cbind(mice::complete(data, action = "long", include = TRUE),
+#                    .mis = factor(rowSums(data$where[, mapping_vrb] > 0), levels = c(0, 1), labels = c("observed", "imputed"), ordered = TRUE))
+# yvar <- if("y" %in% mapping_args) {ggplot2::as_label(mapping$y)} else {ggplot2::as_label(mapping$x)}
--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,5 @@ library(testthat)
 library(ggmice)
 
 test_check("ggmice")
+#TODO: check fully complete data
+#TODO: check fully complete variable
