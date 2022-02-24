2829a8ce39a309273ec19eb187c66d509c6c01b6
hanneoberman <h.i.oberman@uu.nl>
2022-02-24 17:48:33 +0100

    Add -Inf for missing values to show in plot

--- R/ggmice.R
+++ R/ggmice.R
@@ -21,22 +21,26 @@ ggmice <- function(data = NULL, mapping = ggplot2::aes()) {
   # extract variable names from mapping object
   if(mice::is.mids(data)){
     vrbs <- names(data$data)
+    vrbs_num <- vrbs[purrr::map_lgl(data$data, is.numeric)]
   } else {
     vrbs <- names(data)
+    vrbs_num <- vrbs[purrr::map_lgl(data, is.numeric)]
   }
   vrbs_xy <- vrbs[stringr::str_detect(ggplot2::as_label(mapping$x), vrbs) | stringr::str_detect(ggplot2::as_label(mapping$y), vrbs)]
+
   # edit data and mapping objects
   if(mice::is.mids(data)){
-    mice_data <- dplyr::mutate(
+    mice_data <- tidyr::drop_na(dplyr::mutate(
       mice::complete(data, action = "long", include = TRUE),
       .imp = factor(.imp, levels = 0:data$m, labels = c("original", paste("imputation", 1:data$m)), ordered = TRUE),
       .mis = rep(rowSums(as.matrix(data$where[, vrbs_xy])) > 0L, data$m + 1L),
-      .mis = factor(.mis, levels = c(FALSE, TRUE), labels = c("observed", "imputed"), ordered = TRUE))
+      .mis = factor(.mis, levels = c(FALSE, TRUE), labels = c("observed", "imputed"), ordered = TRUE)), vrbs_xy)
     mice_mapping <- utils::modifyList(mapping, ggplot2::aes(colour = .mis, fill = .mis))
   } else {
     mice_data <- dplyr::mutate(
       data,
-      dplyr::across(tidyselect::vars_select_helpers$where(is.numeric), ~tidyr::replace_na(.x, -Inf)),
+      dplyr::across(vrbs_num, ~tidyr::replace_na(.x, -Inf)),
+      dplyr::across(vrbs[vrbs %nin% vrbs_num], ~{as.factor(tidyr::replace_na(as.character(.x), "-Inf"))}),
       .mis = rowSums(is.na(as.matrix(data[, vrbs_xy]))) > 0L,
       .mis = factor(.mis, levels = c(FALSE, TRUE), labels = c("observed", "missing"), ordered = TRUE))
     mice_mapping <- utils::modifyList(mapping, ggplot2::aes(colour = .mis, fill = .mis))
@@ -52,7 +56,7 @@ ggmice <- function(data = NULL, mapping = ggplot2::aes()) {
       ggplot2::facet_wrap(~ .imp)
   }
   # else {
-  #   #annotate("rect", xmin = 3, xmax = 4.2, ymin = 12, ymax = 21, alpha = .2)
+  #   annotate("rect", xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf, alpha = .2)
   # }
   # output
   return(gg)
