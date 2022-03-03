7d38bb9f950e4dce778b2aed253eb9178b486ef6
hanneoberman <h.i.oberman@uu.nl>
2022-03-03 13:50:49 +0100

    Adjust placement of missing data points for categorical variables to match the continues variables

--- tests/testthat/test-ggmice.R
+++ tests/testthat/test-ggmice.R
@@ -4,10 +4,10 @@ test_that("ggmice with continuous data", {
     bmi = bmi,
     .keep = "used"
   )
-  gg <- ggmice(dat, ggplot2::aes("bmi"))
+  gg <- ggmice(dat, ggplot2::aes(bmi))
   expect_s3_class(gg, "ggplot")
   imp <- mice::mice(dat, printFlag = FALSE)
-  gg <- ggmice(imp, ggplot2::aes("bmi"))
+  gg <- ggmice(imp, ggplot2::aes(bmi))
   expect_s3_class(gg, "ggplot")
 })
 
@@ -17,25 +17,27 @@ test_that("ggmice with categorical data", {
     hyp = hyp,
     .keep = "used"
   )
-  gg <- ggmice(dat, ggplot2::aes("bmi"))
+  gg <- ggmice(dat, ggplot2::aes(age))
   expect_s3_class(gg, "ggplot")
   imp <- mice::mice(dat, printFlag = FALSE)
-  gg <- ggmice(imp, ggplot2::aes("bmi"))
+  gg <- ggmice(imp, ggplot2::aes(age))
   expect_s3_class(gg, "ggplot")
 })
 
 test_that("ggmice with complete data", {
   dat <- na.omit(mice::nhanes)
-  gg <- ggmice(dat, ggplot2::aes("bmi"))
+  gg <- ggmice(dat, ggplot2::aes(bmi))
   expect_s3_class(gg, "ggplot")
   imp <- mice::mice(dat, printFlag = FALSE, seed = 1)
-  gg <- ggmice(imp, ggplot2::aes("bmi"))
+  gg <- ggmice(imp, ggplot2::aes(bmi))
   expect_s3_class(gg, "ggplot")
 })
 
 test_that("ggmice with incorrect arguments", {
   dat <- mice::nhanes
-  expect_error(ggmice(as.matrix(dat), ggplot2::aes("bmi")))
-  expect_error(ggmice(dat, ggplot2::aes(shape = "bmi")))
-  expect_warning(ggmice(dat, ggplot2::aes("bmi", color = "bmi")))
+  expect_error(ggmice(as.matrix(dat), ggplot2::aes(bmi)))
+  expect_error(ggmice(dat, ggplot2::aes(shape = bmi)))
+  expect_error(ggmice(dat, ggplot2::aes("bmi")))
+  expect_warning(ggmice(dat, ggplot2::aes(bmi, color = bmi)))
+  expect_error(ggmice(dat, ggplot2::aes(abc)))
 })
