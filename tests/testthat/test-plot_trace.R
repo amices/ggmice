fbcc8c9fb4a9e3ede649f9401a8bfc23bdfb44d8
hanneoberman <h.i.oberman@uu.nl>
2023-02-02 10:31:45 +0100

    Add test to `plot_trace()` for `vrb = "all"` argument

--- tests/testthat/test-plot_trace.R
+++ tests/testthat/test-plot_trace.R
@@ -3,8 +3,9 @@ test_that("plot_trace creates ggplot object", {
   expect_s3_class(plot_trace(imp), "ggplot")
   expect_s3_class(plot_trace(imp, vrb = "bmi"), "ggplot")
   expect_s3_class(plot_trace(imp, vrb = c("bmi", "hyp")), "ggplot")
+  expect_s3_class(plot_trace(imp, vrb = "all"), "ggplot")
   expect_s3_class(plot_trace(imp, vrb = bmi), "ggplot")
-  })
+})
 
 test_that("plot_trace returns error with incorrect argument(s)", {
   imp <- mice::mice(mice::nhanes, printFlag = FALSE)
