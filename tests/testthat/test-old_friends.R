e9514ceea33c41b1f1f08cfc1ab3e4c4d9722e69
hanneoberman <h.i.oberman@uu.nl>
2023-03-09 17:01:55 +0100

    Edit old friends functions to throw an `inform` once per session instead of `message`

--- tests/testthat/test-old_friends.R
+++ tests/testthat/test-old_friends.R
@@ -6,10 +6,10 @@ test_that("set of old friends functions generate plot", {
   expect_s3_class(xyplot(imp, bmi ~ age), "trellis")
 })
 
-test_that("set of old friends functions generate message", {
-  imp <- mice::mice(mice::nhanes, printFlag = FALSE)
-  expect_message(bwplot(imp))
-  expect_message(densityplot(imp))
-  expect_message(stripplot(imp))
-  expect_message(xyplot(imp, bmi ~ age))
-})
+# test_that("set of old friends functions generate message", {
+#   imp <- mice::mice(mice::nhanes, printFlag = FALSE)
+#   expect_message(bwplot(imp))
+#   expect_message(densityplot(imp))
+#   expect_message(stripplot(imp))
+#   expect_message(xyplot(imp, bmi ~ age))
+# })
