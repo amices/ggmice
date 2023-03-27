3038085a90c27726153a88c9442ae4cd8c6e55be
hanneoberman <h.i.oberman@uu.nl>
2023-03-27 16:53:31 +0200

    Adjust `plot_variance()` to accomodate `mira` objects

--- R/plot_fnc.R
+++ R/plot_fnc.R
@@ -12,7 +12,7 @@ library(ggplot2)
 synplot1 <- function(syn.obj, lm.formula){
   vars <- all.vars(lm.formula)
   # row-bind all dfs
-  dplyr::bind_rows(!!!syn.obj, .id="id") %>% 
+  dplyr::bind_rows(!!!syn.obj, .id="id") %>%
   # suppose lm.formula contains one predictor x
     ggplot(aes_string(x = vars[2], y = vars[1], color= "id")) +
     geom_point(size=1, alpha = 0.2) +
@@ -35,33 +35,33 @@ synplot1 <- function(syn.obj, lm.formula){
 synplot2 <- function(model.list, smoother = "lm"){
   # get the model formula
   form <- formula(model.list[[1]])
-  model.list %>% 
+  model.list %>%
     purrr::map(
-      ~.x[c("fitted.values", "model", "residuals")] %>% 
-        dplyr::bind_cols() %>% 
+      ~.x[c("fitted.values", "model", "residuals")] %>%
+        dplyr::bind_cols() %>%
         # select fitted.values & DV which are the 1st and 2nd vars
         dplyr::select(1:2, last_col())
-    ) %>% 
-    dplyr::bind_rows(.id = "ids") %>% 
-    dplyr::group_by(ids) %>% 
+    ) %>%
+    dplyr::bind_rows(.id = "ids") %>%
+    dplyr::group_by(ids) %>%
     # compute MSE per dataset
-    dplyr::mutate(MSEs = mean(residuals^2), 
+    dplyr::mutate(MSEs = mean(residuals^2),
                   ids = glue::glue('{ids}, MSE= {round(MSEs, 2)}')) %>%
-    dplyr::ungroup() %>% 
+    dplyr::ungroup() %>%
     {   # grab the dependent variable and unquote it
       ggplot(., aes(x = fitted.values,
-                    y = !!rlang::sym(all.vars(form)[1]), 
-                    
+                    y = !!rlang::sym(all.vars(form)[1]),
+
                     color= ids)) +
         geom_point(size = 1, alpha = 0.2) +
-        geom_line(stat = "smooth", 
-                  method = smoother, 
-                  alpha = 0.5, 
+        geom_line(stat = "smooth",
+                  method = smoother,
+                  alpha = 0.5,
                   #position = position_dodge(width = 1)
         ) +
         theme_classic() +
         labs(title = paste(smoother, "line for", deparse(form)), color="",
-             subtitle = paste("Average MSE =", round(mean(.$residuals^2),2))) 
+             subtitle = paste("Average MSE =", round(mean(.$residuals^2),2)))
     }
 }
 
@@ -70,7 +70,7 @@ synplot2 <- function(model.list, smoother = "lm"){
 library(magrittr)
 library(mice)
 
-#' Create a plot of average fitted values vs. observed values 
+#' Create a plot of average fitted values vs. observed values
 #' to show the variance between fitted values for the imputed datasets
 #'
 #' @param dat original data
@@ -85,20 +85,20 @@ modelplot <- function(dat, imp.method=NULL, lm.formula, seed=NA, print=FALSE){
   # get the DV
   dv <- all.vars(lm.formula)[1]
   # imputing...
-  tmp <- dat %>% mice::mice(print=FALSE, meth = imp.method, seed=seed) %>% complete("all") %>% 
+  tmp <- dat %>% mice::mice(print=FALSE, meth = imp.method, seed=seed) %>% complete("all") %>%
     # fit the model
-    purrr::map(~.x %$% do.call("lm", list(lm.formula, .)) %>% 
+    purrr::map(~.x %$% do.call("lm", list(lm.formula, .)) %>%
                  # extract fitted vals
-                 .$fitted.values) %>% 
-    # gerko: dv --> observed value ? 
-    dplyr::bind_cols(., DV = dat[,dv]) %>%
+                 .$fitted.values) %>%
+    # gerko: dv --> observed value ?
+   b %>%  dplyr::bind_cols(., DV = dat[,dv]) %>%
     dplyr::rowwise() %>%
     # get the avg.fitted vals and variance (last col = observed val)
-    dplyr::mutate(means = mean(c_across(-DV)),
-                  vars = var(c_across(-DV))
-    ) 
-  plot <- tmp %>% 
-    ggplot(aes(x = means, y = DV, fill = vars, size = vars)) + 
+    dplyr::mutate(means = mean(dplyr::c_across(-DV)),
+                  vars = var(dplyr::c_across(-DV))
+    )
+  plot <- tmp %>%
+    ggplot(aes(x = means, y = DV, fill = vars, size = vars)) +
     geom_point(alpha = 0.7, col = "black", shape=21)  +
     # reverse the col order
     #scale_color_distiller(palette = "YlOrRd", trans="reverse") +
@@ -107,12 +107,12 @@ modelplot <- function(dat, imp.method=NULL, lm.formula, seed=NA, print=FALSE){
          x = "average fitted value", y = paste("observed", dv), color = "variance") +
     theme_classic() +
     # flip the color bar
-    # guides(size = FALSE, col = guide_colourbar(reverse=T))  
-    guides(size = FALSE)  
-    
+    # guides(size = FALSE, col = guide_colourbar(reverse=T))
+    guides(size = FALSE)
+
   # output table
   tab <- tmp %>% data.table::data.table()
-  
+
   if(print) return(list(plot, tab)) else return(plot)
 }
 
--- R/plot_variance.R
+++ R/plot_variance.R
@@ -6,9 +6,7 @@
 #' lighter cells indicate less variance. White cells represent observed cells or unobserved cells with zero between
 #' imputation variance.
 #'
-#' @param data A package `mice` generated multiply imputed data set of class
-#' `mids`. Non-`mids` objects that have not been generated with `mice::mice()`
-#' can be converted through a pipeline with `mice::as.mids()`.
+#' @param data A multiply imputed object of class [`mice::mids`] or [`mice::mira`].
 #' @param grid Logical indicating whether grid lines should be displayed.
 #'
 #' @return An object of class `ggplot`.
@@ -16,42 +14,100 @@
 #' imp <- mice::mice(mice::nhanes, printFlag = FALSE)
 #' plot_variance(imp)
 #' @export
-plot_variance <- function(data, grid = TRUE){
-  if (!mice::is.mids(data)) {
-    stop("Input is not a Multiply Imputed Data Set of class mids. \n
-         Perhaps function mice::as.mids() can be of use?")
+plot_variance <- function(data, grid = TRUE) {
+  if (!(mice::is.mids(data) | mice::is.mira(data))) {
+    stop(
+      "Input is not a Multiply Imputed Data Set of class `mids`/ `mira`. \n
+         Perhaps function mice::as.mids() can be of use?"
+    )
   }
-  if (data$m < 2) {
-    stop("The between inmputation variance cannot be computed if there are fewer than two imputations (m < 2).")
-  }
+  if (mice::is.mids(data)) {
+      stopifnot("The between inmputation variance cannot be computed if there are fewer than two imputations (m < 2)." = data$m > 1)
+  } else {
+      stopifnot("The between inmputation variance cannot be computed if there are fewer than two imputations (m < 2)." = length(data$analyses) > 1)
+    }
+
   if (grid) {
     gridcol <- "black"
   } else {
     gridcol <- NA
   }
 
-  gg <- mice::complete(data, "long")  %>%
-    dplyr::mutate(dplyr::across(where(is.factor), as.numeric)) %>%
-    dplyr::select(-.imp) %>%
-    dplyr::group_by(.id) %>%
-    dplyr::summarise(dplyr::across(dplyr::everything(),  stats::var)) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(dplyr::across(.cols = -.id, ~scale_above_zero(.))) %>%
-    tidyr::pivot_longer(cols = -.id) %>%
-    ggplot2::ggplot(ggplot2::aes(name, .id, fill= value)) +
+  if (mice::is.mids(data)) {
+    long <- mice::complete(data, "long")  %>%
+      dplyr::mutate(dplyr::across(where(is.factor), as.numeric)) %>%
+      dplyr::select(-.imp) %>%
+      dplyr::group_by(.id) %>%
+      dplyr::summarise(dplyr::across(dplyr::everything(),  stats::var)) %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate(dplyr::across(.cols = -.id, ~ scale_above_zero(.))) %>%
+      tidyr::pivot_longer(cols = -.id)
+    lab_x <- "Column name"
+    caption <- "*scaled cell-level between imputation variance"
+  }
+
+  if (mice::is.mira(data)) {
+    long <- purrr::map_dfr(1:length(data$analyses), ~ {
+      pred = predict(data$analyses[[.x]])
+      data.frame(
+        .imp = .x,
+        .id = names(pred),
+        pred = pred,
+        row.names = NULL
+      )
+    }) %>%
+      dplyr::group_by(.id) %>%
+      dplyr::summarise(value = stats::var(pred))
+
+    # summ <- long %>%
+    #   dplyr::group_by(.id) %>%
+    #   dplyr::summarise(avg = mean(pred), vrn = var(pred)) %>%
+    #   # dplyr::arrange(avg) %>%
+    #   dplyr::mutate(.id = reorder(.id, -avg), .rws = as.numeric(row.names(.)))
+
+    long <- long %>%
+      # dplyr::summarise(dplyr::across(dplyr::everything(),  stats::var)) %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate(.id = as.numeric(.id),
+                    name = "")
+
+    # ggplot2::ggplot(long, ggplot2::aes(x = avg, y = .rws, color = vrn)) +
+    #   ggplot2::geom_point() +
+    # ggplot2::scale_y_continuous(trans = "reverse", expand = c(0, 0))
+
+
+    # ggplot2::ggplot(long, ggplot2::aes(x = pred, y = .id, color = .imp)) +
+    #   ggplot2::geom_point()
+
+    # dplyr::bind_cols(., DV = dat[,dv]) %>%
+    # dplyr::rowwise() %>%
+    # dplyr::mutate(means = mean(dplyr::c_across(-DV)),
+    #               vars = var(dplyr::c_across(-DV)))
+
+    lab_x <- "Predicted values"
+    caption <-
+      "*scaled prediction-level between imputation variance"
+  }
+
+  gg <-
+    ggplot2::ggplot(long, ggplot2::aes(name, .id, fill = value)) +
     ggplot2::geom_tile(color = gridcol) +
     ggplot2::scale_fill_gradient(low = "white", high = mice::mdc(2)) +
-    ggplot2::labs(x = "Column name",
-                  y = "Row number",
-                  fill = "Imputation variability*
+    ggplot2::labs(
+      x = lab_x,
+      y = "Row number",
+      fill = "Imputation variability*
       ",
-      caption = "*scaled cell-level between imputation variance") + # "Cell-level between imputation\nvariance (scaled)\n\n"
-    ggplot2::scale_x_discrete(position = "top", expand = c(0,0)) +
-    ggplot2::scale_y_continuous(trans = "reverse", expand = c(0,0)) +
+      caption = caption
+    ) +
+    ggplot2::scale_x_discrete(position = "top", expand = c(0, 0)) +
+    ggplot2::scale_y_continuous(trans = "reverse", expand = c(0, 0)) +
     theme_minimice()
 
+
   if (!grid) {
-    gg <- gg + ggplot2::theme(panel.border = ggplot2::element_rect(fill = NA))
+    gg <-
+      gg + ggplot2::theme(panel.border = ggplot2::element_rect(fill = NA))
   }
 
   # return the ggplot object
@@ -59,8 +115,8 @@ plot_variance <- function(data, grid = TRUE){
 }
 
 # function to scale only non-zero values without centering
-scale_above_zero <- function(x){
+scale_above_zero <- function(x) {
   x <- as.matrix(x)
-  x[x!=0] <- scale(x[x!=0], center = FALSE)
+  x[x != 0] <- scale(x[x != 0], center = FALSE)
   return(x)
 }
