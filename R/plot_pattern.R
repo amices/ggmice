bb692cda4c218ec420f70fc91c08ec08a88b38d2
hanneoberman <h.i.oberman@uu.nl>
2023-04-06 14:50:56 +0200

    Add forgotten comma to plot_pattern()

--- R/plot_pattern.R
+++ R/plot_pattern.R
@@ -63,7 +63,7 @@ plot_pattern <-
           sort(as.numeric(row.names(pat)), decreasing = TRUE)[1:npat]
         rows_pat_full <-
           nrow(pat) # full number of missing data patterns
-        pat <- pat[rownames(pat) %in% c(top_n_pat, ""), drop = FALSE]
+        pat <- pat[rownames(pat) %in% c(top_n_pat, ""), , drop = FALSE]
         # show number of requested, shown, and hidden missing data patterns
         message(
           npat,
